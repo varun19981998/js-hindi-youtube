f3e7946feb8bd770b7f6cebaeea28022dcf87a81
varun pratap singh <84224041+varun19981998@users.noreply.github.com>
2024-08-16 14:37:19 +0530

    Update one.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -23,7 +23,7 @@ const temperature = 41
 
 // const balance = 1000
 
-// if (balance > 500) console.log("test"),console.log("test2");
+// if (balance > 500) console.log("test"),console.log("test2");//implicit scope
 
 // if (balance < 500) {
 //     console.log("less than 500");
